62bd21f15d027f60144a617cac0c43194f72dbac
manuel <manuel@gide.net>
2019-03-10 20:35:11 +0100

    handling sounds

--- src/utils/animationHandler.ts
+++ src/utils/animationHandler.ts
@@ -1,3 +1,5 @@
+import {store} from '../config/store';
+
 export function walkAnimation(caracter, path0x, path1x, path0y, path1y) {
     if (path0x / 32 * 32 > path1x / 32 * 32 && path0y / 16 * 16 > path1y / 16 * 16) {
         caracter.animations.play('west', 10, false);
@@ -39,22 +41,25 @@ export function fightAnimation(caracter, ennemy) {
 }
 
 export function swordSound(sword1, sword2, pare) {
-    const rd = Math.random();
-    switch (Math.round(rd * 10)) {
-        case 1:
-        case 2:
-        case 3:
-            sword1.play();
-            break;
-        case 4:
-        case 5:
-        case 6:
-            sword2.play();
-            break;
-        case 7:
-        case 8:
-        case 9:
-            pare.play();
-            break;
+    if (store.sound) {
+        const rd = Math.random();
+
+        switch (Math.round(rd * 10)) {
+            case 1:
+            case 2:
+            case 3:
+                sword1.play();
+                break;
+            case 4:
+            case 5:
+            case 6:
+                sword2.play();
+                break;
+            case 7:
+            case 8:
+            case 9:
+                pare.play();
+                break;
+        }
     }
 }
