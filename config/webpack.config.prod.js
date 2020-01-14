41c14fa4c5af8a0896e884dbe28f0bf8c9ec94ac
manuellandreau <manuel.landreau@gide.net>
2020-01-14 15:29:30 +0100

    quickfix webpack compat ok

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -165,7 +165,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
@@ -278,7 +278,7 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
-    new InterpolateHtmlPlugin(env.raw),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
